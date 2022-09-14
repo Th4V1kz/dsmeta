c36923afa9dc35ef92f46a84cea3038389f5735a
Th4V1kz <r-victorrosa@hotmail.com>
2022-09-14 19:17:16 -0300

    API test

---
+++ backend/src/main/java/com/Thevikz/dsmeta/repositories/SaleRepository.java
@@ -0,0 +1,10 @@
+package com.Thevikz.dsmeta.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.Thevikz.dsmeta.entities.Sale;
+
+public interface SaleRepository extends JpaRepository<Sale, Long>  {
+
+}
+
